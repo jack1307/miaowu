2ba17fcc232bae7b9335ee70778bccc5ada65d04
wangfugui <15988831887@163.com>
2024-05-11 21:48:12 +0800

    系统服务

--- miaowu-module-system/miaowu-system-busi/src/main/java/com/wang/SystemServerApplication.java
+++ miaowu-module-system/miaowu-system-busi/src/main/java/com/wang/SystemServerApplication.java
@@ -1,5 +1,6 @@
 package com.wang;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -11,6 +12,5 @@ public class SystemServerApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+        SpringApplication.run(SystemServerApplication.class, args);    }
 }
